ad446542ede48a3c39e3f96df664b443f1e639a4
Shivangi Kumari <12shivangikumari@gmail.com>
2025-09-01 01:58:18 +0530

    added psp

--- Recursion/Q13_PrintStairPaths.java
+++ Recursion/Q13_PrintStairPaths.java
@@ -1,25 +1,25 @@
-import java.util.*; 
+import java.util.*;
+
 public class Q13_PrintStairPaths {
-  public static void main (String [] args){
+  public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
-    int n = scn.nextInt();
-    ArrayList <String> cont = new ArrayList<>();
-    gsp(n, "", cont); 
+    int n = scn.nextInt(); // 3
+    ArrayList<String> cont = new ArrayList<>();
+    psp(n, "", cont);
     System.out.println(cont);
   }
 
-  public static void gsp(int n, String bres, ArrayList <String> cont){
-    if(n == 0){
+  public static void psp(int n, String bres, ArrayList<String> cont) {
+    if (n == 0) {
       // System.out.print(bres + ",");
       cont.add(bres);
-    }
-    else if(n < 0){
+    } else if (n < 0) {
       return;
     }
-    
-    gsp(n-1, bres + "1", cont);
-    gsp(n-2, bres + "2", cont);
-    gsp(n-3, bres + "3", cont);
+
+    psp(n - 1, bres + "1", cont);
+    psp(n - 2, bres + "2", cont);
+    psp(n - 3, bres + "3", cont);
   }
-  
+
 }
