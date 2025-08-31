cf8b06c9f0fd5d43a05f23d52576cee26fc62d5d
Shivangi Kumari <12shivangikumari@gmail.com>
2025-08-31 16:58:59 +0530

    added loop examples

--- Recursion/Q9_GetSubsequence.java
+++ Recursion/Q9_GetSubsequence.java
@@ -1,30 +1,31 @@
 import java.util.*;
+
 public class Q9_GetSubsequence {
-  public static void main (String [] args) {
-    Scanner scn = new Scanner (System.in);
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
     String input = scn.nextLine(); // abc
 
-    ArrayList <String> ans = gss(input); // [___, __c, _b_, _bc, a__, a_c, ab_, abc]
+    ArrayList<String> ans = gss(input); // [___, __c, _b_, _bc, a__, a_c, ab_, abc]
     System.out.println(ans); // bheja normal string tha aaya array mai bhar ke
   }
 
-  public static ArrayList <String> gss(String input){
-    if(input.length() == 0){
-      ArrayList <String> newarr = new ArrayList<>();
+  public static ArrayList<String> gss(String input) {
+    if (input.length() == 0) {
+      ArrayList<String> newarr = new ArrayList<>();
       newarr.add("");
       return newarr;
     }
 
     char ch = input.charAt(0); // a
     String ros = input.substring(1); // bc
-    ArrayList <String> rres = gss(ros);
-    ArrayList <String> ans = new ArrayList<>();
+    ArrayList<String> rres = gss(ros);
+    ArrayList<String> ans = new ArrayList<>();
 
-    for(String abc : rres){
+    for (String abc : rres) { // abc catches each String element
       ans.add("_" + abc);
     }
 
-    for(String abc : rres){
+    for (String abc : rres) {
       ans.add(ch + abc);
     }
 
@@ -36,5 +37,6 @@ public class Q9_GetSubsequence {
 // c = _ , c
 // bc = _ _ , _ c , b _ , b c (c mai 2 loop chlega ek nhi aane ka ek aane ka)
 // abc = _ _ _ , _ _ c , _ b _ , _ b c
-//       a _ _ , a _ c , a b _ , a b c (bc mai 2 loop chlega ek nhi aane ka ek aane ka)
-// 
+// a _ _ , a _ c , a b _ , a b c (bc mai 2 loop chlega ek nhi aane ka ek aane
+// ka)
+//
