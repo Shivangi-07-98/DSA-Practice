d484a3b45a81962eb55f62d1789258deb9b761e7
Shivangi Kumari <12shivangikumari@gmail.com>
2025-09-02 01:46:55 +0530

    updated get print kpc

--- Recursion/Q16_GetKpc.java
+++ Recursion/Q16_GetKpc.java
@@ -1,45 +1,51 @@
 import java.util.*;
 public class Q16_GetKpc {
-   public static void main (String [] args){
+
+  public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
-    String n = scn.nextLine(); // input in numbers eg- 96
+    String str = scn.nextLine(); // input in numbers 96
 
-    ArrayList <String> ans = getkpc(n);
+    ArrayList<String> ans = getkpc(str); // 96
     System.out.println(ans);
-   }
+  }
 
-   public static String[] codes = {".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"}; // from 0-9
+  public static String[] codes = { ".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz" }; // from 0-9
 
-   public static ArrayList <String> getkpc (String n) {
-    if(n.length() == 0){
-      ArrayList <String> bres = new ArrayList <>();
+  public static ArrayList<String> getkpc(String str) {
+    if (str.length() == 0) {
+      ArrayList<String> bres = new ArrayList<>();
       bres.add("");
       return bres;
     }
 
-    char ch = n.charAt(0);
-    String ros = n.substring(1);
-    ArrayList <String> rres = getkpc(ros); 
-    ArrayList <String> newal = new ArrayList<>();
+    char ch = str.charAt(0); // 9
+    String ros = str.substring(1); // 6
+    ArrayList<String> rres = getkpc(ros);
+    ArrayList<String> newal = new ArrayList<>();
 
-    String code = codes[ch - '0'];
- 
-    for(int i = 0; i < code.length(); i++){
+    String code = codes[ch - '0']; // 9
+    // System.out.println('9' - '0');
+    // String code2 = codes[Character.getNumericValue(ch)];
+
+    for (int i = 0; i < code.length(); i++) {
       char chi = code.charAt(i);
-      for(String rstr: rres){
+      for (String rstr : rres) {
         newal.add(chi + rstr);
       }
     }
 
-    // Bad Loop
-
-    // for(String res: rres){
-    //   for(int i = 0; i < code.length(); i++){
-    //     char chi = code.charAt(i);
-    //     newal.add(chi + res);
-    //   }
-    // }
-
     return newal;
   }
 }
+
+// '0' - '0' is 0
+// '1' - '0' is 1
+// '2' - '0' is 2
+// '3' - '0' is 3
+// '4' - '0' is 4
+// '5' - '0' is 5
+// '6' - '0' is 6
+// '7' - '0' is 7
+// '8' - '0' is 8
+// '9' - '0' is 9
+// System.out.println('9' - '0');
