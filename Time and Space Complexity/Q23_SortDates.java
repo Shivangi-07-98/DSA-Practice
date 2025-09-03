25261356d5fd723d4967f87b50508d7396d49820
Shivangi Kumari <12shivangikumari@gmail.com>
2025-09-04 03:13:49 +0530

    sort dates

--- Time and Space Complexity/Q23_SortDates.java
+++ Time and Space Complexity/Q23_SortDates.java
@@ -1,30 +1,30 @@
-// SortDatesQ
+// 
 import java.util.*;
 public class Q23_SortDates {
+
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt(); // 3
     String[] arr = new String[n];
     for (int i = 0; i < arr.length; i++) {
-      String str = scn.next(); // 13051997 25041997 13071997
-      arr[i] = str;
+      arr[i] = scn.next();
+      // String str = scn.next(); // 13051997 25041997 13071997
+      // arr[i] = str;
     }
-
     SortDates(arr);
     print(arr);
   }
 
-  public static void SortDates(String[] arr) {
+  public static void SortDates(String[] arr) { // call in sorting order
     // dates
-    CountSort(arr, 1000000, 100, 32);
+    CountSort(arr, 1000000, 100, 32); // 0 to 31 indexes in farr
     // month
-    CountSort(arr, 10000, 100, 13);
+    CountSort(arr, 10000, 100, 13); // 0 to 12 indexes in farr
     // year
-    CountSort(arr, 1, 10000, 2501);
+    CountSort(arr, 1, 10000, 2501); // 0 to 2500 indexes in farr
   }
 
   public static void CountSort(String[] arr, int div, int mod, int range) {
-
     int[] farr = new int[range];
     String[] ans = new String[arr.length];
 
@@ -50,7 +50,6 @@ public class Q23_SortDates {
     for (int i = 0; i < arr.length; i++) {
       arr[i] = ans[i];
     }
-
   }
 
   public static void print(String[] arr) {
