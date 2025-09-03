74580b7f9e277efe2ade764e96b6f18d1f6bf2b9
Shivangi Kumari <12shivangikumari@gmail.com>
2025-09-04 03:37:51 +0530

    print permutations

--- Recursion/Q20_PrintPermutations.java
+++ Recursion/Q20_PrintPermutations.java
@@ -1,27 +1,28 @@
-import java.util.*; 
+import java.util.*;
 public class Q20_PrintPermutations {
-  public static void main (String [] args){
+  
+  public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
     String str = scn.nextLine(); // abc
 
-    ArrayList <String> cont = new ArrayList<>();
-    PrintPermutations(str, "", cont); 
-    System.out.print(cont);
+    ArrayList<String> cont = new ArrayList<>();
+    PrintPermutations(str, "", cont); // xyz xzy yxz yzx zxy zyx
+    System.out.println(cont);
   }
 
-  public static void PrintPermutations(String str, String bres, ArrayList <String> cont){
-    if(str.length() == 0){
+  public static void PrintPermutations(String str, String bres, ArrayList<String> cont) {
+    if (str.length() == 0) {
       // System.out.print(bres + ",");
       cont.add(bres);
       return;
     }
 
-    for(int i = 0; i < str.length(); i++){
+    for (int i = 0; i < str.length(); i++) {
       char ch = str.charAt(i);
       String left = str.substring(0, i);
       String right = str.substring(i + 1);
       PrintPermutations(left + right, bres + ch, cont);
     }
   }
-  
+
 }
