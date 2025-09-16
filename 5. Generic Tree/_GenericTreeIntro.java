62d50a4af7ecc7d6ef53884c1a748c61b3084075
Shivangi Kumari <12shivangikumari@gmail.com>
2025-09-16 18:35:23 +0530

    added notes

--- 5. Generic Tree/_GenericTreeIntro.java
+++ 5. Generic Tree/_GenericTreeIntro.java
@@ -1,127 +1,45 @@
 import java.util.*;
 
-public class _GenericTreeIntro { 
+public class _GenericTreeIntro {
 
-    public static class Node {
-        int data;
-        ArrayList<Node> children = new ArrayList<>();
+  public static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
 
-        // constructor
-        Node(int data) {
-            this.data = data;
-        }
+    // constructor
+    Node(int data) {
+      this.data = data;
     }
+  }
 
-    // display 1 => 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120
-    // using queue
-    public static void levelOrder(Node root) {
-        Queue<Node> queue = new ArrayDeque<Node>();
-        queue.add(root);
+  public static void main(String[] args) {
+    Node root = new Node(10); // value daal paa rhe hai because of constructor
+    Node twenty = new Node(20);
+    Node thirty = new Node(30);
+    Node forty = new Node(40);
+    Node fifty = new Node(50);
+    Node sixty = new Node(60);
+    Node seventy = new Node(70);
+    Node eighty = new Node(80);
+    Node hundredten = new Node(110);
+    Node hundred = new Node(100);
+    Node ninety = new Node(90);
+    Node hundredtwenty = new Node(120);
 
-        while (queue.size() > 0) {
-            // remove, print, add children
-            Node temp = queue.remove();
-            System.out.print(temp.data + " ");
-            for (Node child : temp.children) {
-                queue.add(child);
-            }
-        }
+    root.children.add(twenty);
+    root.children.add(thirty);
+    root.children.add(forty);
+    twenty.children.add(fifty);
+    twenty.children.add(sixty);
+    thirty.children.add(seventy);
+    thirty.children.add(eighty);
+    thirty.children.add(ninety);
+    forty.children.add(hundred);
+    eighty.children.add(hundredten);
+    eighty.children.add(hundredtwenty);
 
-        System.out.println();
-    }
-
-    // display 2 =>
-    // 10
-    // 20 30 40
-    // 50 60 70 80 90 100
-    // 110 120
-    public static void levelOrderLinewise(Node root) {
-        Queue<Node> queue = new ArrayDeque<Node>();
-        Queue<Node> cqueue = new ArrayDeque<Node>();
-        queue.add(root);
-
-        while (queue.size() > 0) {
-            Node temp = queue.remove();
-            System.out.print(temp.data + " ");
-
-            for (Node child : temp.children) {
-                cqueue.add(child);
-            }
-
-            if (queue.size() == 0) {
-                queue = cqueue;
-                cqueue = new ArrayDeque<>();
-                System.out.println();
-            }
-        }
-    }
-
-    public static void myLevelOrderLinewise(Node root) {
-        Queue<Node> one = new ArrayDeque<Node>();
-        Queue<Node> two = new ArrayDeque<Node>();
-        one.add(root);
-        while (one.size() > 0) {
-            Node temp = one.remove();
-            System.out.print(temp.data + " ");
-            for (Node val : temp.children) {
-                two.add(val);
-            }
-
-            if (one.size() == 0) {
-                one = two;
-                two = new ArrayDeque<>();
-                System.out.println();
-            }
-
-        }
-
-    }
-
-    // display 3
-    public static void levelOrderLinewiseZigZag(Node root) {
-
-    }
-
-    public static void main(String[] args) {
-        Node root = new Node(10);
-
-        Node twenty = new Node(20);
-        root.children.add(twenty);
-
-        Node thirty = new Node(30);
-        root.children.add(thirty);
-
-        Node forty = new Node(40);
-        root.children.add(forty);
-
-        Node fifty = new Node(50);
-        twenty.children.add(fifty);
-
-        Node sixty = new Node(60);
-        twenty.children.add(sixty);
-
-        Node seventy = new Node(70);
-        thirty.children.add(seventy); 
-
-        Node eighty = new Node(80);
-        thirty.children.add(eighty);
-
-        Node ninety = new Node(90);
-        thirty.children.add(ninety);
-
-        Node hundred = new Node(100);
-        forty.children.add(hundred);
-
-        Node hundredten = new Node(110);
-        eighty.children.add(hundredten);
-
-        Node hundredtwenty = new Node(120);
-        eighty.children.add(hundredtwenty);
-
-        // levelOrder(root);
-        // levelOrderLinewise(root);
-        myLevelOrderLinewise(root);
-    }
+    // levelOrder(root);
+    // levelOrderLinewise(root);
+  }
 
 }
- 
--- 5. Generic Tree/_GenericTreeIntro2.java
+++ 5. Generic Tree/_GenericTreeIntro2.java
@@ -1,83 +1,89 @@
 import java.util.*;
 
-public class _GenericTreeIntro2 { 
+public class _GenericTreeIntro2 {
 
-    public static class Node {
-        int data;
-        ArrayList<Node> children = new ArrayList<>();
+  public static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
 
-        Node(int data) {
-            this.data = data;
-        }
+    Node(int data) {
+      this.data = data;
+    }
+  }
+
+  // display 1 => 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120
+  // using queue
+  public static void levelOrder(Node root) {
+    Queue<Node> queue = new ArrayDeque<Node>();
+    queue.add(root);
+
+    while (queue.size() > 0) {
+      // remove, print, add children
+      Node temp = queue.remove();
+      System.out.print(temp.data + " ");
+      for (Node child : temp.children) {
+        queue.add(child);
+      }
     }
 
-    public static void levelOrder(Node root) {
-        Queue<Node> queue = new ArrayDeque<Node>();
-        queue.add(root);
+    System.out.println();
+  }
 
-        while (queue.size() > 0) {
-            // remove, print, add children
-            Node temp = queue.remove();
-            System.out.print(temp.data + " ");
-            for (Node child : temp.children) {
-                queue.add(child);
-            }
-        }
+  // display 2 =>
+  // 10
+  // 20 30 40
+  // 50 60 70 80 90 100
+  // 110 120
+  public static void levelOrderLinewise(Node root) {
+    Queue<Node> queue = new ArrayDeque<Node>();
+    Queue<Node> cqueue = new ArrayDeque<Node>();
+    queue.add(root);
 
+    while (queue.size() > 0) {
+      Node temp = queue.remove();
+      System.out.print(temp.data + " ");
+
+      for (Node child : temp.children) {
+        cqueue.add(child);
+      }
+
+      if (queue.size() == 0) {
+        queue = cqueue;
+        cqueue = new ArrayDeque<>();
         System.out.println();
+      }
     }
+  }
 
-    public static void levelOrderLinewise(Node root) {
-        Queue<Node> queue = new ArrayDeque<Node>();
-        Queue<Node> cqueue = new ArrayDeque<Node>();
-        queue.add(root);
+  public static Node construct(int[] arr) {
+    // using the array create a tree and return root
+    Node root = null;
+    Stack<Node> stack = new Stack<>();
 
-        while (queue.size() > 0) {
-            Node temp = queue.remove();
-            System.out.print(temp.data + " ");
+    for (int val : arr) {
+      if (val != -1) {
+        Node node = new Node(val);
+        stack.push(node);
+      } else {
+        Node node = stack.pop();
 
-            for (Node child : temp.children) {
-                cqueue.add(child);
-            }
-
-            if (queue.size() == 0) {
-                queue = cqueue;
-                cqueue = new ArrayDeque<>();
-                System.out.println();
-            }
+        if (stack.size() > 0) {
+          Node parent = stack.peek();
+          parent.children.add(node);
+        } else {
+          root = node;
         }
+      }
     }
+    return root;
+  }
 
-    public static Node construct(int[] arr) {
-        // using the array create a tree and return root
-        Node root = null;
-        Stack<Node> stack = new Stack<>();
+  public static void main(String[] args) {
+    int[] arr = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1 };
+    Node root = construct(arr);
 
-        for (int val : arr) {
-            if (val != -1) {
-                Node node = new Node(val);
-                stack.push(node);
-            } else {
-                Node node = stack.pop();
+    // levelOrder(root);
+    levelOrderLinewise(root);
+  }
 
-                if (stack.size() > 0) {
-                    Node parent = stack.peek();
-                    parent.children.add(node);
-                } else {
-                    root = node;
-                }
-            }
-        }
-        return root;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1 };
-        Node root = construct(arr);
-
-        // levelOrder(root);
-        levelOrderLinewise(root);
-    }
- 
 }
- 
