12172e1379d84b2866ee6a46be0812cbb83c1f76
Shivangi Kumari <12shivangikumari@gmail.com>
2025-09-18 03:44:42 +0530

    Traversals Pre Post Order

--- 5. Generic Tree/TraversalsPrePostOrderQ.java
+++ 5. Generic Tree/TraversalsPrePostOrderQ.java
@@ -2,113 +2,75 @@ import java.util.*;
 
 public class TraversalsPrePostOrderQ {
 
-    private static class Node {
-        int data;
-        ArrayList<Node> children = new ArrayList<>();
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+
+    Node(int data) {
+      this.data = data;
     }
+  }
 
-    public static void display(Node node) {
-        String str = node.data + " -> ";
-        for (Node child : node.children) {
-            str += child.data + ", ";
-        }
-        str += ".";
-        System.out.println(str);
+  public static void levelOrderLinewise(Node root) {
+    Queue<Node> queue = new ArrayDeque<Node>();
+    Queue<Node> cqueue = new ArrayDeque<Node>();
+    queue.add(root);
 
-        for (Node child : node.children) {
-            display(child);
-        }
-    }
-
-    public static void levelOrder(Node root) {
-        Queue<Node> queue = new ArrayDeque<Node>();
-        queue.add(root);
-
-        while (queue.size() > 0) {
-            // remove, print, add children
-            Node temp = queue.remove();
-            System.out.print(temp.data + " ");
-            for (Node child : temp.children) {
-                queue.add(child);
-            }
-        }
-
+    while (queue.size() > 0) {
+      Node temp = queue.remove();
+      System.out.print(temp.data + " ");
+      for (Node child : temp.children) {
+        cqueue.add(child);
+      }
+      if (queue.size() == 0) {
+        queue = cqueue;
+        cqueue = new ArrayDeque<>();
         System.out.println();
+      }
     }
+  }
 
-    public static void levelOrderLinewise(Node root) {
-        Queue<Node> queue = new ArrayDeque<Node>();
-        Queue<Node> cqueue = new ArrayDeque<Node>();
-        queue.add(root);
-
-        while (queue.size() > 0) {
-            Node temp = queue.remove();
-            System.out.print(temp.data + " ");
-
-            for (Node child : temp.children) {
-                cqueue.add(child);
-            }
-
-            if (queue.size() == 0) {
-                queue = cqueue;
-                cqueue = new ArrayDeque<>();
-                System.out.println();
-            }
+  public static Node construct(int[] arr) {
+    // using the array create a tree and return root
+    Node root = null;
+    Stack<Node> stack = new Stack<>();
+    for (int val : arr) {
+      if (val != -1) {
+        Node node = new Node(val);
+        stack.push(node);
+      } else {
+        Node node = stack.pop();
+        if (stack.size() > 0) {
+          Node parent = stack.peek();
+          parent.children.add(node);
+        } else {
+          root = node;
         }
+      }
+    }
+    return root;
+  }
+
+  public static void traversals(Node node) {
+    // Node Pre
+    System.out.println("Node Pre " + node.data);
+
+    // Loop, before call is Edge Pre, after call is Edge Post
+    for (Node child : node.children) {
+      System.out.println("Edge Pre " + node.data + "--" + child.data);
+      traversals(child);
+      System.out.println("Edge Post " + node.data + "--" + child.data);
     }
 
-    public static Node construct(int[] arr) {
-        Node root = null;
+    // Node Post
+    System.out.println("Node Post " + node.data);
+  }
 
-        Stack<Node> st = new Stack<>();
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == -1) {
-                st.pop();
-            } else {
-                Node t = new Node();
-                t.data = arr[i];
-
-                if (st.size() > 0) {
-                    st.peek().children.add(t);
-                } else {
-                    root = t;
-                }
-                st.push(t);
-            }
-        }
-
-        return root;
-    }
-
-    public static void traversals(Node node) {
-        // Node Pre
-        System.out.println("Node Pre " + node.data);
-
-        // Loop, before call is Edge Pre, after call is Edge Post
-        for (Node child : node.children) {
-            System.out.println("Edge Pre " + node.data + "--" + child.data);
-            traversals(child);
-            System.out.println("Edge Post " + node.data + "--" + child.data);
-        }
-
-        // Node Post
-        System.out.println("Node Post " + node.data);
-    }
-
-    public static void myTraversals(Node root){
-
-    }
-
-
-    public static void main(String[] args) throws Exception {
-        int[] arr = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1,
-                -1 };
-
-        Node root = construct(arr);
-        traversals(root);
-        // myTraversals(root);
-    }
+  public static void main(String[] args) throws Exception {
+    int[] arr = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1 };
+    Node root = construct(arr);
+    traversals(root);
+  }
 
 }
 
