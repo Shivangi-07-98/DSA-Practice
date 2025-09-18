5c5e5d3cc0600dfc8e9fb7f138c632bab1b616fe
Shivangi Kumari <12shivangikumari@gmail.com>
2025-09-19 04:41:35 +0530

    linearize efficient

--- 5. Generic Tree/Q7_Linearize.java
+++ 5. Generic Tree/Q7_Linearize.java
@@ -10,6 +10,22 @@ public class Q7_Linearize {
     }
   }
 
+  public static void levelOrder(Node root) {
+    Queue<Node> queue = new ArrayDeque<Node>();
+    queue.add(root);
+
+    while (queue.size() > 0) {
+      // remove, print, add children
+      Node temp = queue.remove();
+      System.out.print(temp.data + " ");
+      for (Node child : temp.children) {
+        queue.add(child);
+      }
+    }
+
+    System.out.println();
+  }
+
   public static void levelOrderLinewise(Node root) {
     Queue<Node> queue = new ArrayDeque<Node>();
     Queue<Node> cqueue = new ArrayDeque<Node>();
@@ -59,6 +75,7 @@ public class Q7_Linearize {
     return node;
   }
 
+  // O(n^2)
   public static void linearize(Node node) {
     for (Node child : node.children) {
       linearize(child);
@@ -71,19 +88,7 @@ public class Q7_Linearize {
     }
   }
 
-  public static void myLinearize(Node node) {
-    for (int i = node.children.size() - 1; i >= 0; i--) {
-      if (node.children.size() > 1) {
-        Node val = node.children.remove(i);
-        Node parent = node.children.get(i - 1);
-        parent.children.add(val);
-      }
-    }
-    for (Node child : node.children) {
-      myLinearize(child);
-    }
-  }
-
+  // O(n)
   // linearizes and returns the tail
   public static Node linearizeEfficient(Node node) {
     if (node.children.size() == 0) {
@@ -103,13 +108,13 @@ public class Q7_Linearize {
 
     return lastKiTail;
   }
-  
+
   public static void main(String[] args) throws Exception {
     int[] arr = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1,
         -1 };
     Node root = construct(arr);
 
-    // linearize(root);
+    // myLinearize(root);
     // levelOrderLinewise(root);
 
     linearizeEfficient(root);
