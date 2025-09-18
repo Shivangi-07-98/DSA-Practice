0521ebaa00516e3f0a2b6197d2f1df9afb4999be
Shivangi Kumari <12shivangikumari@gmail.com>
2025-09-19 05:09:49 +0530

    find in gt

--- 5. Generic Tree/Q8_FindInGt.java
+++ 5. Generic Tree/Q8_FindInGt.java
@@ -2,117 +2,94 @@ import java.util.*;
 
 public class Q8_FindInGt {
 
-    public static class Node {
-        int data;
-        ArrayList<Node> children = new ArrayList<>();
+  public static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
 
-        Node(int data) {
-            this.data = data;
-        }
+    Node(int data) {
+      this.data = data;
     }
+  }
 
-    public static void levelOrder(Node root) {
-        Queue<Node> queue = new ArrayDeque<Node>();
-        queue.add(root);
+  public static void levelOrder(Node root) {
+    Queue<Node> queue = new ArrayDeque<Node>();
+    queue.add(root);
+    while (queue.size() > 0) {
+      // remove, print, add
+      Node temp = queue.remove();
+      System.out.print(temp.data + " ");
+      for (Node child : temp.children) {
+        queue.add(child);
+      }
+    }
+    System.out.println(".");
+  }
 
-        while (queue.size() > 0) {
-            // remove, print, add
-            Node temp = queue.remove();
-            System.out.print(temp.data + " ");
-            for (Node child : temp.children) {
-                queue.add(child);
-            }
-        }
+  public static void levelOrderLinewise(Node root) {
+    Queue<Node> queue = new ArrayDeque<Node>();
+    Queue<Node> cqueue = new ArrayDeque<Node>();
+    queue.add(root);
 
+    while (queue.size() > 0) {
+      Node temp = queue.remove();
+      System.out.print(temp.data + " ");
+
+      for (Node child : temp.children) {
+        cqueue.add(child);
+      }
+
+      if (queue.size() == 0) {
+        queue = cqueue;
+        cqueue = new ArrayDeque<>();
         System.out.println(".");
+      }
     }
+  }
 
-    public static void levelOrderLinewise(Node root) {
-        Queue<Node> queue = new ArrayDeque<Node>();
-        Queue<Node> cqueue = new ArrayDeque<Node>();
-        queue.add(root);
+  public static Node construct(int[] arr) {
+    // using the array create a tree and return root
+    Node root = null;
+    Stack<Node> stack = new Stack<>();
 
-        while (queue.size() > 0) {
-            Node temp = queue.remove();
-            System.out.print(temp.data + " ");
+    for (int val : arr) {
+      if (val != -1) {
+        Node node = new Node(val);
+        stack.push(node);
+      } else {
+        Node node = stack.pop();
 
-            for (Node child : temp.children) {
-                cqueue.add(child);
-            }
-
-            if (queue.size() == 0) {
-                queue = cqueue;
-                cqueue = new ArrayDeque<>();
-                System.out.println(".");
-            }
+        if (stack.size() > 0) {
+          Node parent = stack.peek();
+          parent.children.add(node);
+        } else {
+          root = node;
         }
+      }
     }
+    return root;
+  }
 
-    public static Node construct(int[] arr) {
-        // using the array create a tree and return root
-        Node root = null;
-        Stack<Node> stack = new Stack<>();
-
-        for (int val : arr) {
-            if (val != -1) {
-                Node node = new Node(val);
-                stack.push(node);
-            } else {
-                Node node = stack.pop();
-
-                if (stack.size() > 0) {
-                    Node parent = stack.peek();
-                    parent.children.add(node);
-                } else {
-                    root = node;
-                }
-            }
-        }
-        return root;
+  public static boolean find(Node node, int data) {
+    if (node.data == data) {
+      return true;
     }
-
-    public static boolean find(Node node, int data) {
-        if (node.data == data) {
-            return true;
-        }
-
-        for (Node child : node.children) {
-            boolean fic = find(child, data);
-            if (fic == true) {
-                return true;
-            }
-        }
-
-        return false;
+    for (Node child : node.children) {
+      boolean fic = find(child, data);
+      if (fic == true) {
+        return true;
+      }
     }
+    return false;
+  }
 
-    public static boolean myFind(Node node, int data){
-        // boolean has = false;
-        if(node.data == data){
-            return true;
-        }
-        for(Node child: node.children){
-            boolean has = find(child, data);
-            if(has == true){
-                return true;
-            }
-        }
+  public static void main(String[] args) {
+    int[] arr = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1,
+        -1 };
+    Node root = construct(arr);
 
-        return false;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1,
-                -1 };
-        Node root = construct(arr);
-
-        int data = 1200;
-
-        boolean flag = myFind(root, data);
-        System.out.println(flag);
-
-        // levelOrder(root);
-        // levelOrderLinewise(root);
-    }
+    int data = 1200;
+    boolean flag = find(root, data);
+    System.out.println(flag);
+  }
 
 }
