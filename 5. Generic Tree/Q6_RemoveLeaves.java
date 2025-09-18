b19bf48d77d210e713c016980f9223b013fad3b7
Shivangi Kumari <12shivangikumari@gmail.com>
2025-09-18 20:39:51 +0530

    remove leaves in gt

--- 5. Generic Tree/Q6_RemoveLeaves.java
+++ 5. Generic Tree/Q6_RemoveLeaves.java
@@ -10,19 +10,6 @@ public class Q6_RemoveLeaves {
     }
   }
 
-  public static void display(Node node) {
-    String str = node.data + " -> ";
-    for (Node child : node.children) {
-      str += child.data + ", ";
-    }
-    str += ".";
-    System.out.println(str);
-
-    for (Node child : node.children) {
-      display(child);
-    }
-  }
-
   public static void levelOrderLinewise(Node root) {
     Queue<Node> queue = new ArrayDeque<Node>();
     Queue<Node> cqueue = new ArrayDeque<Node>();
@@ -67,7 +54,7 @@ public class Q6_RemoveLeaves {
 
   public static void removeLeaves(Node node) {
     // remove your own leaves
-    // deleting in node pre also this loop should be above the call
+    // deleting in node pre
     for (int i = node.children.size() - 1; i >= 0; i--) {
       Node child = node.children.get(i);
       if (child.children.size() == 0) {
@@ -81,20 +68,6 @@ public class Q6_RemoveLeaves {
     }
   }
 
-  public static void myRemoveLeaves(Node node) {
-    for (int i = node.children.size() - 1; i >= 0; i--) {
-      Node child = node.children.get(i);
-      if (child.children.size() == 0) {
-        node.children.remove(i);
-      }
-    }
-
-    for(Node child: node.children){
-      myRemoveLeaves(child);
-    }
-    
-  }
-
   public static void main(String[] args) throws Exception {
     int[] arr = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1,
         -1 };
@@ -102,7 +75,6 @@ public class Q6_RemoveLeaves {
 
     levelOrderLinewise(root);
     removeLeaves(root);
-    // display(root);
     levelOrderLinewise(root);
   }
 
