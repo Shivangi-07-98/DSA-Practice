d9ddee565f3b35d450ce1e5c15b53b1e8c73805e
Shivangi Kumari <12shivangikumari@gmail.com>
2025-09-22 14:43:03 +0530

    predecessor and successor of an element

--- 5. Generic Tree/Q20_PredecessorAndSuccessor.java
+++ 5. Generic Tree/Q20_PredecessorAndSuccessor.java
@@ -96,11 +96,11 @@ public class Q20_PredecessorAndSuccessor {
     int[] arr = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1,
         -1 };
     Node root = construct(arr);
-    int data = 110;
 
-    predecessor = null;
-    successor = null;
-    state = 0;
+    int data = 110;
+    // predecessor = null;
+    // successor = null;
+    // state = 0;
     predecessorAndSuccessor(root, data);
 
     System.out.println(predecessor == null ? "Predecessor = Not found" : "Predecessor = " + predecessor.data);
